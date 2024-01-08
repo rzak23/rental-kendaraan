7a505630037882cdf83eeb445757213001a16122
rzak23 <reza.akbar2396@gmail.com>
2024-01-08 10:14:23 +0800

    set env loader

--- index.php
+++ index.php
@@ -312,4 +312,12 @@ switch (ENVIRONMENT)
  *
  * And away we go...
  */
+require_once 'vendor/autoload.php';
+$repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
+    ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
+    ->addWriter(Dotenv\Repository\Adapter\PutenvAdapter::class)
+    ->immutable()
+    ->make();
+$dotenv = Dotenv\Dotenv::create($repository, './');
+$dotenv->safeLoad();
 require_once BASEPATH.'core/CodeIgniter.php';
